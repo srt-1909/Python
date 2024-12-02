288d35b6f8e772ed94249b34088e6d4a988f0782
srt-1909 <sachinthakkar1992002@gmail.com>
2024-12-03 00:03:02 +0530

    input in python

---
+++ Input.py
@@ -0,0 +1,11 @@
+# string input
+name = input("name : ")
+print(name)
+
+#number input
+age = int(input("age : "))
+print(age)
+
+#float input
+networth = float(input("networth : "))
+print(networth)
