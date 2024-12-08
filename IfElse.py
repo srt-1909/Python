25da775d719672cd36c2abb1f671ef7edbe2e4b0
srt-1909 <sachinthakkar1992002@gmail.com>
2024-12-08 23:40:25 +0530

    operators

--- IfElse.py
+++ IfElse.py
@@ -1,3 +1,17 @@
-food = input("food : ")
-eat = "yes" if food == "cake" else "no"
-print(eat) 
+# food = input("food : ")
+# eat = "yes" if food == "cake" else "no"
+# print(eat) 
+
+
+# age = int(input("age : "))
+
+# vote = ("yes", "no")  [age >= 18] 
+
+# print(vote) 
+
+
+salary = float(input("salary :"))
+tax = salary*(0.1, 0.2) [salary > 50000]
+print(tax)
+
+
---
+++ Operators.py
@@ -0,0 +1,6 @@
+# assignment operators
+
+num = 10
+# num = num +10
+num += 10
+print("num :", num)
