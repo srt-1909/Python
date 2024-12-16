e4fb7a59cfc1bfa1457561b821627ee360c67705
srt-1909 <sachinthakkar1992002@gmail.com>
2024-12-16 13:06:51 +0530

    list tuples

--- ListInPython.py
+++ ListInPython.py
@@ -1,8 +1,17 @@
 marks = [23.5, 54.6, 22.1]
+marks.sort()
 print(marks)
 print(type(marks))
 
 
 
 student = ["Sachin", 54.6, 22.1]
+print(student)
+student[0] = "kgf"
+
+print(student)
+
+
+student.append(55)
+
 print(student)
---
+++ ListQuestion.py
@@ -0,0 +1,12 @@
+list1 = ["m","a", "a", "m"]
+
+copy_list1 = list1.copy()
+
+copy_list1.reverse()
+
+if(copy_list1 == list1):
+    print("palindrome")
+else:{
+    print("not palindrome")
+}
+    
